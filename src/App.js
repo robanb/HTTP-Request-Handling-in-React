888a1950071e7691618260b9efc43ccaa5e07478
Roba Nath Basnet <robanbth@gmail.com>
2022-11-24 02:25:12 +0600

    Added firebase endpoint

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useCallback } from "react";
 
 import MoviesList from "./components/MoviesList";
+import AddMovie from "./components/AddMovie";
 import "./App.css";
 
 function App() {
@@ -12,10 +13,13 @@ function App() {
 		setIsLoading(true);
 		setError(null);
 		try {
-			const response = await fetch("https://swapi.dev/api/films/");
+			const response = await fetch(
+				"https://react-http-caeb0-default-rtdb.asia-southeast1.firebasedatabase.app/movies.json"
+			);
 			if (!response.ok) {
-				throw new Error("Something went Wrong!");
+				throw new Error("Something went wrong!");
 			}
+
 			const data = await response.json();
 
 			const transformedMovies = data.results.map((movieData) => {
@@ -32,23 +36,34 @@ function App() {
 		}
 		setIsLoading(false);
 	}, []);
+
 	useEffect(() => {
 		fetchMoviesHandler();
-	}, []);
+	}, [fetchMoviesHandler]);
 
-	let content = <p>Found no movies</p>;
+	function addMovieHandler(movie) {
+		console.log(movie);
+	}
+
+	let content = <p>Found no movies.</p>;
+
 	if (movies.length > 0) {
 		content = <MoviesList movies={movies} />;
 	}
+
 	if (error) {
 		content = <p>{error}</p>;
 	}
+
 	if (isLoading) {
 		content = <p>Loading...</p>;
 	}
 
 	return (
 		<React.Fragment>
+			<section>
+				<AddMovie onAddMovie={addMovieHandler} />
+			</section>
 			<section>
 				<button onClick={fetchMoviesHandler}>Fetch Movies</button>
 			</section>
