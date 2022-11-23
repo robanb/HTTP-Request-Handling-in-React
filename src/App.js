86a1f1937c021cea2ecbdd29d9dd8c822986da28
Roba Nath Basnet <robanbth@gmail.com>
2022-11-24 02:48:51 +0600

    POST request added and tested

--- src/App.js
+++ src/App.js
@@ -21,16 +21,17 @@ function App() {
 			}
 
 			const data = await response.json();
+			const loadedMovies = [];
+			for (const key in data) {
+				loadedMovies.push({
+					id: key,
+					title: data[key].title,
+					openingText: data[key].openingText,
+					releaseDate: data[key].releaseDate,
+				});
+			}
 
-			const transformedMovies = data.results.map((movieData) => {
-				return {
-					id: movieData.episode_id,
-					title: movieData.title,
-					openingText: movieData.opening_crawl,
-					releaseDate: movieData.release_date,
-				};
-			});
-			setMovies(transformedMovies);
+			setMovies(loadedMovies);
 		} catch (error) {
 			setError(error.message);
 		}
@@ -41,8 +42,19 @@ function App() {
 		fetchMoviesHandler();
 	}, [fetchMoviesHandler]);
 
-	function addMovieHandler(movie) {
-		console.log(movie);
+	async function addMovieHandler(movie) {
+		const response = await fetch(
+			"https://react-http-caeb0-default-rtdb.asia-southeast1.firebasedatabase.app/movies.json",
+			{
+				method: "POST",
+				body: JSON.stringify(movie),
+				headers: {
+					"Content-Type": "application/json",
+				},
+			}
+		);
+		const data = await response.json();
+		console.log(data);
 	}
 
 	let content = <p>Found no movies.</p>;
